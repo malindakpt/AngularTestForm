56f55dcd63a808a1bc624b657d0502f89fa81c05
MalindaK <m.malinda@mubasher.net>
2018-05-31 12:20:44 +0530

    added initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,56 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatSliderModule,
+  MatSnackBarModule,
+  MatTooltipModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { PersonDetailComponent } from './components/person-detail/person-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    BrowserAnimationsModule,
+    FormsModule,
+
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatGridListModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatListModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
